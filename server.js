01ce1e98e8a27f8b83093ac14b669c7613470a71
xfibben <87906057+xfibben@users.noreply.github.com>
2022-10-30 15:10:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express=require('express');
 const { Server } = require('http');
 const app= express();
 app.get('/',(req,res)=>{res.send("conectado correctamente")})
-app.listen(3000);
-console.log('Server on port 3000')
+app.listen(8080);
+console.log('Server on port 8080')
